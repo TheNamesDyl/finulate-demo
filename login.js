e9e542a73f90d6727fcdd9c47d615ccd72788f54
Kathy Xu <yuanyx2c>
2019-03-11 20:40:49 -0700

    hover images

--- login.js
+++ login.js
@@ -3,7 +3,7 @@
 
   window.onload = function() {
     
-    );
+    ;
   };
 
   function currentScreen(beforeScreen, afterScreen) {
@@ -11,6 +11,19 @@
     beforeScreen.style.visibility = "hidden";
     afterScreen.style.visibility = "visible";
   }
+
+  $("subscrip").onmouseover = function(){
+    $("sub_customer").style.visibility = "visible";
+  };
+
+  // $("subscrip").addEventListener("click", function() {
+  //   alert("hi");
+  //   $("Group_add").style.visibility = "visible";
+  // });
+
+  function displayText() {
+     $("Group_add").style.visibility = "visible";
+}
 
   function $(id) {
     return document.getElementById(id);
